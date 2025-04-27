51ef266e2b3ac12b585f42807088934cf84d3878
dparekh22 <dparekh.official@gmail.com>
2025-04-27 02:18:37 -0500

    Fixed DB connection environment variables

--- projects/tcg-leaderboard/database.py
+++ projects/tcg-leaderboard/database.py
@@ -2,9 +2,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
 
 # Create DB URL string
-DB_URL = 'postgresql://luffy:mugiwara@localhost:5432/tcg_leaderboard'
+DB_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/tcg_leaderboard'
 
 # Create engine with DB_URL
 engine = create_engine(DB_URL)
